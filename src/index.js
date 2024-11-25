2a1c115a4fbf2f8a972835c51adca69b8a90df2a
Veronica Prado Vargas <vprado@iriscx.com>
2024-11-25 02:26:48 -0400

    removing unnecesary lines

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ async function main() {
     await sequelize.sync({ force: false });
     const port = process.env.PORT;
     app.listen(port);
-    logger.info(`Server is running on port ${port}`);
-    logger.error(`Server is running on port ${port}`);
-    logger.debug(`Server is running on port ${port}`);
-    logger.warn(`Server is running on port ${port}`);
-    logger.fatal(`Server is running on port ${port}`);
+
 }
 main();
